e6c737599433ec805b78af8739c7ab41f1055769
Dax <dax.rocha@hotmail.com>
2024-09-05 21:31:01 +0100

    View Holder Recycler View Adapter Set Adapter

---
+++ app/src/main/java/com/devspace/recyclerview/Contact.kt
@@ -0,0 +1,10 @@
+package com.devspace.recyclerview
+
+import androidx.annotation.DrawableRes
+
+class Contact(
+    val name: String,
+    val phone: String,
+    @DrawableRes val icon: Int
+)
+
--- app/src/main/java/com/devspace/recyclerview/MainActivity.kt
+++ app/src/main/java/com/devspace/recyclerview/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,6 +20,100 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        // Set adapter
+        // Linear Layout Manager
+        // Submeter a lista
+
+
+        val rvList = findViewById<RecyclerView>(R.id.rv_list)
+        val adapter = ContactListAdapter()
+
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+        adapter.submitList(contacts)
 
     }
 }
+
+val contacts = listOf(
+    Contact(
+        name = "Dax",
+        phone = "+351 931 322 434",
+        icon = R.drawable.sample1
+    ),
+    Contact(
+        name = "Dax",
+        phone = "+351 931 322 434",
+        icon = R.drawable.sample2
+    ),
+    Contact(
+        name = "Dax",
+        phone = "+351 931 322 434",
+        icon = R.drawable.sample3
+    ),
+    Contact(
+        name = "Dax",
+        phone = "+351 931 322 434",
+        icon = R.drawable.sample4
+    ),
+    Contact(
+        name = "Dax",
+        phone = "+351 931 322 434",
+        icon = R.drawable.sample5
+    ),
+    Contact(
+        name = "Dax",
+        phone = "+351 931 322 434",
+        icon = R.drawable.sample6
+    ),
+    Contact(
+        name = "Dax",
+        phone = "+351 931 322 434",
+        icon = R.drawable.sample7
+    ),
+    Contact(
+        name = "Dax",
+        phone = "+351 931 322 434",
+        icon = R.drawable.sample8
+    ),
+    Contact(
+        name = "Dax",
+        phone = "+351 931 322 434",
+        icon = R.drawable.sample9
+    ),
+    Contact(
+        name = "Dax",
+        phone = "+351 931 322 434",
+        icon = R.drawable.sample10
+    ),
+    Contact(
+        name = "Dax",
+        phone = "+351 931 322 434",
+        icon = R.drawable.sample11
+    ),
+    Contact(
+        name = "Dax",
+        phone = "+351 931 322 434",
+        icon = R.drawable.sample12
+    ),
+    Contact(
+        name = "Dax",
+        phone = "+351 931 322 434",
+        icon = R.drawable.sample13
+    ),
+    Contact(
+        name = "Dax",
+        phone = "+351 931 322 434",
+        icon = R.drawable.sample14
+    ),
+    Contact(
+        name = "Dax",
+        phone = "+351 931 322 434",
+        icon = R.drawable.sample15
+    ),
+    Contact(
+        name = "Dax",
+        phone = "+351 931 322 434",
+        icon = R.drawable.sample16
+    )
+)
